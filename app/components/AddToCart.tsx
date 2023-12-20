8fde9af8c4b11d6ecdfc8717dbb9e70d1efb91cf
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-12-19 21:43:39 -0500

    next js tutorial part 1

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,9 @@
+'use client'
+const AddToCart = () => {
+    return (
+        <div className="btn btn-primary">
+            <button onClick={()=>console.log("click")}>Add to cart</button>
+        </div>
+    )
+}
+export default AddToCart
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import AddToCart from "@/app/components/AddToCart";
+
+
+
+export const ProductCard = () => {
+    return (
+        <div >
+            {/*this is a client component*/}
+            <AddToCart />
+        </div>
+    )
+}
