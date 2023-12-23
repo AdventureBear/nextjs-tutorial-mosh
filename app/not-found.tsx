035f20d25f755033df35e951dcde70fa1793b5fe
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-12-22 20:04:48 -0500

    Handle Not Found Errors

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+    return (
+        <div>The requested page doesn't exist</div>
+    )
+}
+export default NotFoundPage
---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const UserNotFoundPage = () => {
+    return (
+        <div>This user doesn't exist</div>
+    )
+}
+export default UserNotFoundPage
